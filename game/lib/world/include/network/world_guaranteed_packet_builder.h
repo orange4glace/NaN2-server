4ca8c5262b779eb545bc28fe9ceb2412bc196d5b
orange4glace <orange4glace@gmail.com>
2017-05-27 16:37:18 +0900

    fix typo

--- game/lib/world/include/network/world_guaranteed_packet_builder.h
+++ game/lib/world/include/network/world_guaranteed_packet_builder.h
@@ -3,7 +3,7 @@
 
 #include "packet_builder.h"
 
-#include "flatbuffers/world_guaranteed_generated.h"
+#include "../flatbuffers/world_guaranteed_generated.h"
 #include "entity_created_packet.h"
 #include "entity_destroied_packet.h"
 
